bed00a252c47425cee982b71a4081843cf6384d0
Rohit Nag <rnag2018@gmail.com>
2022-11-29 18:00:59 +0000

    Added a trimesh stl reader, with sectioning ability. Modified ct scan plots.

--- scan_analyser.py
+++ scan_analyser.py
@@ -6,7 +6,6 @@ import os
 
 # load the DICOM files
 path = '/Users/rohit/Documents/Imperial/ME4/FYP/Sample Scans/MJM09_MJM010/MJM09_MJM010_Phantom1607,2003840n/R4G1B43W/TDS102WE'
-directory = os.fsencode(path)
 files = []
 
 with os.scandir(path) as it:
@@ -24,54 +23,34 @@ for f in files:
     if hasattr(f, 'SliceLocation') and f.ImageOrientationPatient == [1, 0, 0, 0, 1, 0]:
         slices.append(f)
     else:
-        skipcount = skipcount + 1
+        skipcount += 1
 
 print("skipped, no SliceLocation: {}".format(skipcount))
 
 
 # ensure they are in the correct order
 slices = sorted(slices, key=lambda s: s.SliceLocation)
-
-print(f'slices[0].SliceLocation: {slices[0].SliceLocation}')
-print(f'slices[0].SliceThickness: {slices[0].SliceThickness}')
-print(f'slices[0].RotationDirection: {slices[0].RotationDirection}')
-print(
-    f'slices[0].ImageOrientationPatient: {slices[0].ImageOrientationPatient}')
 print(f'slices[0].ImagePositionPatient: {slices[0].ImagePositionPatient}')
-
-
-print(f'slices[-1].SliceLocation: {slices[-1].SliceLocation}')
-print(f'slices[-1].SliceThickness: {slices[-1].SliceThickness}')
-print(f'slices[-1].RotationDirection: {slices[-1].RotationDirection}')
-print(
-    f'slices[-1].ImageOrientationPatient: {slices[-1].ImageOrientationPatient}')
 print(f'slices[-1].ImagePositionPatient: {slices[-1].ImagePositionPatient}')
 
-
-true_z = np.linspace(slices[0].SliceLocation,
-                     slices[-1].SliceLocation,
-                     len(slices),
-                     endpoint=True)
+min_true_z, max_true_z = slices[0].SliceLocation, slices[-1].SliceLocation
+print(f'min_true_z: {min_true_z}')
+z = np.linspace(0,
+                max_true_z - min_true_z,
+                len(slices),
+                endpoint=True)
 
 # pixel aspects, assuming all slices are the same
 ps = slices[0].PixelSpacing
-print(f'ps: {ps}')
 ss = slices[0].SliceThickness
-print(f'ss: {ss}')
 ax_aspect = ps[1]/ps[0]
-print(f'ax_aspect: {ax_aspect}')
 sag_aspect = ps[1]/ss
-print(sag_aspect)
 cor_aspect = ps[0]/ss
-print(cor_aspect)
 
 # create 3D array
 img_shape = slices[0].pixel_array.shape + (len(slices), )
 img3d = np.zeros(img_shape)
 
-
-# points = []
-# densities = []
 # fill 3D array with the images from the files
 avg_densities = np.zeros(len(slices))
 for k, slice in enumerate(slices):
@@ -79,95 +58,86 @@ for k, slice in enumerate(slices):
     img3d[:, :, k] = cross_section
     avg_densities[k] = np.mean(cross_section)
 
-x = np.linspace(0, ps[0]*img_shape[0], img_shape[0], endpoint=False)
-y = np.linspace(0, ps[1]*img_shape[1], img_shape[1], endpoint=False)
-z = np.linspace(0, ss*img_shape[2], img_shape[2], endpoint=False)
-
 avg_densities_grad = np.gradient(avg_densities)
 z_cutoff = (np.argmax(avg_densities_grad) + 10,
             np.argmin(avg_densities_grad) - 10)
 
-filtered_avg_densities_grad3 = np.gradient(avg_densities_grad)
-filtered_avg_densities_grad3[:z_cutoff[0]] = 0
-filtered_avg_densities_grad3[z_cutoff[1]:] = 0
+filtered_avg_densities_grad2 = np.gradient(avg_densities_grad)
+filtered_avg_densities_grad2[:z_cutoff[0]] = 0
+filtered_avg_densities_grad2[z_cutoff[1]:] = 0
 
-
-# plot 3 orthogonal slices
-fig, ax = plt.subplots(2, 3)
-fig.tight_layout()
-fig.suptitle('Planar Slices Pixel Data')
-# # adjust the main plot to make room for the sliders
-# fig.subplots_adjust(left=0.25, bottom=0.25)
-# x_cut = img_shape[0]//2
+# Plots
 x_cut = 300
 x_cut_color = 'orange'
 y_cut = img_shape[1]//2
 y_cut_color = 'lime'
 # z_cut = img_shape[2]//2
-z_cut = int(np.argmax(filtered_avg_densities_grad3) + slices[0].SliceLocation)
-max_grad3 = max(filtered_avg_densities_grad3)
-print(max_grad3)
+z_cut = np.argmax(filtered_avg_densities_grad2)
+z_cut = 200
+max_grad3 = max(filtered_avg_densities_grad2)
 z_cut_color = 'red'
 
-# # Make a horizontal slider to control the z_cut.
-# axz = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-# z_slider = Slider(
-#     ax=axz,
-#     label='Axial cut (mm)',
-#     valmin=0,
-#     valmax=200,
-#     valinit=z_cut,
-# )
+fig = plt.figure(layout="constrained")
+subfigs = fig.subfigures(1, 2, wspace=0, width_ratios=[2, 1])
+subfigs[0].set_facecolor('0.9')
+subfigs[0].suptitle(f'Raw CT Scan Pixel Data')
+axs0 = subfigs[0].subplots()
+
+axial_img = img3d[:, :, z_cut]
+axs0.imshow(axial_img,
+            origin='lower',
+            aspect=ax_aspect,
+            extent=[0, axial_img.shape[0]*ps[0], 0, axial_img.shape[1]*ps[1]])
+axs0.set_title(f'Axial Plane, z={z_cut}', c=z_cut_color)
+axs0.set_xlabel('x (mm)')
+axs0.set_ylabel('y (mm)')
+axs0.axhline(y_cut, c=y_cut_color, alpha=0.5)
+axs0.axvline(x_cut, c=x_cut_color, alpha=0.5)
+
+subfigs[1].suptitle('Density variations in z-dir')
+subfigs[1].supxlabel('z (mm)')
+axs1 = subfigs[1].subplots(5, sharex=True)
+
 cor_img = img3d[x_cut, :, :]
-ax[0, 0].imshow(cor_img, origin='lower', aspect=cor_aspect, extent=[
-                min(true_z), max(true_z), cor_img.shape[0], 0])
-ax[0, 0].set_title(f'Coronal Plane, x={x_cut}', c=x_cut_color)
-ax[0, 0].set_xlabel('z (mm)')
-ax[0, 0].set_ylabel('y (mm)')
-ax[0, 0].axvline(z_cut, c=z_cut_color, alpha=0.5)
-ax[0, 0].axhline(y_cut, c=y_cut_color, alpha=0.5)
+axs1[0].imshow(cor_img,
+               origin='lower',
+               aspect='auto',
+               extent=[0, cor_img.shape[1]*ss, 0, cor_img.shape[0]*ps[1]])
+axs1[0].set_title(f'Coronal Plane, x={x_cut}', c=x_cut_color)
+axs1[0].set_ylabel('y (mm)')
+axs1[0].axvline(z_cut, c=z_cut_color, alpha=0.5)
+axs1[0].axhline(y_cut, c=y_cut_color, alpha=0.5)
 
 sag_img = img3d[:, y_cut, :]
-ax[0, 1].imshow(sag_img, origin='lower', aspect=sag_aspect, extent=[
-                min(true_z), max(true_z), sag_img.shape[0], 0])
-ax[0, 1].set_title(f'Sagittal Plane, y={y_cut}', c=y_cut_color)
-ax[0, 1].set_xlabel('z (mm)')
-ax[0, 1].set_ylabel('x (mm)')
-ax[0, 1].axhline(x_cut, c=x_cut_color, alpha=0.5)
-ax[0, 1].axvline(z_cut, c=z_cut_color, alpha=0.5)
+axs1[1].imshow(sag_img,
+               origin='lower',
+               aspect='auto',
+               extent=[0, cor_img.shape[1]*ss, 0, cor_img.shape[0]*ps[0]])
+axs1[1].set_title(f'Sagittal Plane, y={y_cut}', c=y_cut_color)
+axs1[1].axvline(z_cut, c=z_cut_color, alpha=0.5)
+axs1[1].axhline(x_cut, c=x_cut_color, alpha=0.5)
 
-ax[0, 2].imshow(img3d[:, :, z_cut], aspect=ax_aspect)
-ax[0, 2].set_title(f'Axial Plane, z={z_cut}', c=z_cut_color)
-ax[0, 2].set_xlabel('x (mm)')
-ax[0, 2].set_ylabel('y (mm)')
-ax[0, 2].axhline(y_cut, c=y_cut_color, alpha=0.5)
-ax[0, 2].axvline(x_cut, c=x_cut_color, alpha=0.5)
+axs1[2].set_title('Axial Cross-sectional Pixel Intensity')
+axs1[2].set_ylabel("p(x,y)")
+axs1[2].set_xlim([min(z), max(z)])
+axs1[2].plot(z, avg_densities)
+axs1[2].axvline(z_cut, c=z_cut_color, alpha=0.5)
+
+axs1[3].set_title('Axial Cross-sectional Pixel Intensity 1st Derivative')
+axs1[3].set_ylabel("p'(x,y)")
+axs1[3].set_xlim([min(z), max(z)])
+axs1[3].plot(z, avg_densities_grad)
+axs1[3].axvline(z_cut, c=z_cut_color, alpha=0.5)
 
 
-ax[1, 0].set_title('Axial Cross-sectional Pixel Intensity')
-ax[1, 0].set_ylabel("p(x,y)")
-ax[1, 0].set_xlabel('z (mm)')
-ax[1, 0].set_xlim([min(true_z), max(true_z)])
-ax[1, 0].plot(true_z, avg_densities)
-ax[1, 0].axvline(z_cut, c=z_cut_color, alpha=0.5)
+axs1[4].set_title(
+    'Filtered Axial Cross-sectional Pixel Intensity 2nd Derivative')
+axs1[4].set_ylabel("p''(x,y)")
+axs1[4].set_xlim([min(z), max(z)])
+axs1[4].plot(z, filtered_avg_densities_grad2)
+axs1[4].axvline(z_cut, c=z_cut_color, alpha=0.5)
+axs1[4].axvspan(min(z), z_cutoff[0], alpha=0.1, color='black')
+axs1[4].axvspan(z_cutoff[1], max(z), alpha=0.1, color='black')
 
-ax[1, 1].set_title('Axial Cross-sectional Pixel Intensity 1st Derivative')
-ax[1, 1].set_ylabel("p'(x,y)")
-ax[1, 1].set_xlabel('z (mm)')
-ax[1, 1].set_xlim([min(true_z), max(true_z)])
-ax[1, 1].plot(true_z, avg_densities_grad)
-ax[1, 1].axvline(z_cut, c=z_cut_color, alpha=0.5)
-
-ax[1, 2].set_title(
-    'Filtered Axial Cross-sectional Pixel Intensity 3rd Derivative')
-ax[1, 2].set_ylabel("p'''(x,y)")
-ax[1, 2].set_xlabel('z (mm)')
-ax[1, 2].set_xlim([min(true_z), max(true_z)])
-ax[1, 2].plot(true_z, filtered_avg_densities_grad3)
-ax[1, 2].axvline(z_cut, c=z_cut_color, alpha=0.5)
-ax[1, 2].axvspan(min(true_z), z_cutoff[0] +
-                 slices[0].SliceLocation, alpha=0.1, color='black')
-ax[1, 2].axvspan(z_cutoff[1] + slices[0].SliceLocation,
-                 max(true_z), alpha=0.1, color='black')
 
 plt.show()
--- stl_analyser.py
+++ stl_analyser.py
@@ -1,54 +1,54 @@
+import matplotlib
+from mpl_toolkits import mplot3d
+from stl.mesh import Mesh
 import matplotlib.pyplot as plt
 import numpy as np
-from stl.mesh import Mesh
-from mpl_toolkits import mplot3d
-import matplotlib
-
-# # Create a new plot
-# figure = plt.figure()
-# axes = mplot3d.Axes3D(figure)
-
-# # Load the STL files and add the vectors to the plot
-# your_mesh = mesh.Mesh.from_file(
-#     '/Users/rohit/Documents/Imperial/ME4/FYP/Sample Scans/MJM09_MJM010/MJM09_2003840N_Left Tibia.stl')
-# axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-# # Auto scale to the mesh size
-# scale = your_mesh.points.flatten()
-# axes.auto_scale_xyz(scale, scale, scale)
-
-# # Show the plot to the screen
-# # plt.savefig('myfilename.png')
-# plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# x = np.linspace(-2, 2, 60)
-# y = np.linspace(-2, 2, 60)
-# x, y = np.meshgrid(x, y)
-# r = np.sqrt(x**2 + y**2)
-# z = np.cos(r)
-# surf = ax.plot_surface(x, y, z, rstride=2, cstride=2,
-#                        cmap='viridis', linewidth=0)
-
 ss = 0.6
+origin = np.array([-190.630859375, -341.130859375, -84])
 
-mesh = Mesh.from_file(
+lt_m = Mesh.from_file(
     '/Users/rohit/Documents/Imperial/ME4/FYP/Sample Scans/MJM09_MJM010/MJM09_2003840N_Left Tibia.stl')
-min_z, max_z = np.min(mesh.z), np.max(mesh.z)
-print(f'min_z, max_z: {min_z, max_z}')
-height = max_z - min_z
-no_slices = np.floor(height / ss) + 1
-print(f'no_slices: {no_slices}')
-# mesh.translate(np.array([0, 0, -np.min(mesh.z)]))
-knee_loc = np.max(mesh.z)
-print(f'knee_loc: {knee_loc}')
-ax.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors))
-scale = mesh.points.flatten()
+rt_m = Mesh.from_file(
+    '/Users/rohit/Documents/Imperial/ME4/FYP/Sample Scans/MJM09_MJM010/MJM10_2003840N_Right Tibia.stl')
+lt_min_z, lt_maz_z = np.min(lt_m.z), np.max(lt_m.z)
+rt_min_z, rt_maz_z = np.min(rt_m.z), np.max(rt_m.z)
+print(f'lt_min_z, lt_max_z: {lt_min_z, lt_maz_z}')
+print(f'rt_min_z, rt_max_z: {lt_min_z, lt_maz_z}')
+lt_height = lt_maz_z - lt_min_z
+rt_height = rt_maz_z - rt_min_z
+lt_no_slices = np.floor(lt_height / ss) + 1
+rt_no_slices = np.floor(rt_height / ss) + 1
+print(f'lt_no_slices: {lt_no_slices}')
+print(f'rt_no_slices: {rt_no_slices}')
+lt_m.translate(origin * -1)
+rt_m.translate(origin * -1)
+lt_knee_loc = np.max(lt_m.z)
+rt_knee_loc = np.max(rt_m.z)
+print(f'lt_knee_loc: {lt_knee_loc}')
+print(f'rt_knee_loc: {rt_knee_loc}')
+ax.add_collection3d(
+    mplot3d.art3d.Poly3DCollection(
+        lt_m.vectors,
+        edgecolor='#0015FF16',
+        facecolor='#0015FF15',
+        alpha=0.4,
+        label='left tibia')
+)
+ax.add_collection3d(
+    mplot3d.art3d.Poly3DCollection(
+        rt_m.vectors,
+        edgecolor='#FF00001C',
+        facecolor='#FF000012',
+        alpha=0.4,
+        label='right tibia')
+)
+scale = lt_m.points.flatten()
 ax.auto_scale_xyz(scale, scale, scale)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-
 plt.show()
